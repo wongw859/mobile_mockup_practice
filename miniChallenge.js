e681103313b4bf4e93e37571a21cb4b96be854bc
wongw859 <wongw859@gmail.com>
2021-01-06 11:47:10 +0800

    commit

---
+++ miniChallenge.js
@@ -0,0 +1,16 @@
+$('document').ready(function() {
+    // const productListEnglish = ['Advanced Night Repair', 'Advanced Night Repair', 'Micro Essense', 'Revitalizing Supreme +', 'Advanced Night Repair', 'Advanced Night Repair', 'Nutritious', 'Double Wear'];
+    // const productListChinese = ['白金系列', '面部修護系列', '面部修護系列','面部修護系列','面部修護系列','面部修護系列','面部修護系列'];
+    // const imgText = document.querySelector('.imgText');
+    const imgList = document.querySelectorAll('.imgList');
+    // imgList.forEach((item, index) => {
+    //     if (index % 2 === 0) {
+    //         item[index].style.background = "#a88b32";
+    //     }
+    //     item[index].style.background = "#000";
+    // });
+  
+    // imgText.innerHTML=productListEnglish.map((item)=>{
+    //   return '<p>'+item+'</p>';
+    // }).join('');
+})
